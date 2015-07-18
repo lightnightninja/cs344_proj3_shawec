a2bd5dda6d02ef34186824f575b538f677042d72
lightnightninja <mywho09@gmail.com>
2015-07-18 15:10:02 -0700

    Initial Commit
    Not much to see here, just setting up the first file.

---
+++ shell.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cs344_proj3_shawec
+//
+//  Created by Cierra Shawe on 7/16/15.
+//  Copyright (c) 2015 Cierra Shawe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
