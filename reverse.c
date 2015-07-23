e9b581b7dacb67e833487bda618b745fa29d4d30
lightnightninja <mywho09@gmail.com>
2015-07-22 22:31:55 -0700

    Ok, finally. Strrev code cited, not a linux function.

--- reverse.c
+++ reverse.c
@@ -7,16 +7,34 @@
 //  Created by Cierra Shawe on 7/22/15.
 //  Copyright (c) 2015 Cierra Shawe. All rights reserved.
 //
+
+
 #include <stdio.h>
 #include <string.h>
+
+/* No wonder it wasn't working, strrev isn't in linux, whoops. Downside of testing on a mac.
+ code from: http://stackoverflow.com/questions/8534274/is-the-strrev-function-not-available-in-linux
+ */
+char *strrev(char *str){
+    char *p1, *p2;
+
+    if (! str || ! *str)
+        return str;
+    for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2){
+        *p1 ^= *p2;
+        *p2 ^= *p1;
+        *p1 ^= *p2;
+    }
+    return str;
+}
 
 int main(){
 
     char buffer[128];
-
+    char *ret;
     fgets(buffer, 128, stdin);
-    strrev(buffer);
-    fputs(buffer, stdout);
+    ret = strrev(buffer);
+    fputs(ret, stdout);
 
     return 0;
 
