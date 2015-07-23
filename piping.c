5916fc1d6a01aba5475a2c65a3b49115a953dffe
lightnightninja <mywho09@gmail.com>
2015-07-22 22:18:23 -0700

    Modified piping example to test

--- piping.c
+++ piping.c
@@ -1,3 +1,70 @@
-#include <stdio.h>
+/* NOTE THIS IS STILL ALMOST THE EXACT CODE FROM THE BOOK, STILL WORKING WITH IT. */
 
-void hello(){}
+
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+
+#define BUF_SIZE 10
+
+int piper(char **args, char **envp, int after) {
+    int pfd[2];                             /* Pipe file descriptors */
+    char buf[BUF_SIZE];
+    ssize_t numRead;
+    int status = 0;
+
+    if (pipe(pfd) == -1){                    /* Create the pipe */
+        perror("error opening pipe");
+        status = -1;
+    }
+
+    switch (fork()) {
+        case -1:
+            perror("fork");
+            status = -1;
+            break;
+
+        case 0:             /* Child  - reads from pipe */
+            if (close(pfd[1]) == -1){
+                perror("couldn't shut the child away (aka close the fd)");
+                break;
+            }
+
+            for (;;) {              /* Read data from pipe, echo on stdout */
+                numRead = read(pfd[0], buf, BUF_SIZE);
+                if (numRead == -1)
+                    perror("read");
+                if (numRead == 0)
+                    break;                      /* End-of-file */
+                if (write(STDOUT_FILENO, buf, numRead) != numRead)
+                    perror("child - partial/failed write");
+            }
+
+            write(STDOUT_FILENO, "\n", 1);
+            if (close(pfd[0]) == -1){
+                perror("close");
+                status = -1;
+            }
+            break;
+
+        default:            /* Parent - writes to pipe */
+            if (close(pfd[0]) == -1)            /* Read end is unused */
+                perror("close - parent");
+
+            if (write(pfd[1], args[1], strlen(args[1])) != strlen(args[1]))
+                perror("parent - partial/failed write");
+
+            if (close(pfd[1]) == -1)
+                perror("The parent didn't want to stop.");
+            wait(NULL); //that child better hurry up.
+            status = -1;
+
+    }
+
+    return status;
+}
